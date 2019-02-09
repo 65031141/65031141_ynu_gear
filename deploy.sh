659d6fbe6e7c6624203c14a249860ee6ba15d26c
thiswind <23764+thiswind@users.noreply.github.com>
2019-02-09 14:15:29 +0800

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+git clone https://github.com/65031141/65031141.github.io.git
+cd 65031141.github.io
+git checkout gh-pages
+cd ..
+
+while [ 0 ]
+do
+  cd 65031141.github.io
+  git pull
+  cd ..
+
+  mkdir -p html
+  rm -rf html/*
+
+  cp -r 65031141.github.io/* html/
+
+  rm -rf html/.git
+  rm -rf html/.circleci
+  rm -rf html/.gitignore
+
+  echo "updated at $(date)"
+
+  sleep 1
+done
