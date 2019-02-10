38c6d97d43f958286c821d74c47ac228e0fd876e
thiswind <23764+thiswind@users.noreply.github.com>
2019-02-11 04:42:55 +0800

    fix and refactoring

--- deploy.sh
+++ deploy.sh
@@ -1,32 +1,43 @@
 #!/bin/bash
 
-cd /usr/share/
+# get into working dir
+cd /usr/share/nginx/
 
-rm -rf html/*
+# clear repo dir if exists
+rm -rf 65031141.github.io
 
+# clone site
 git clone https://github.com/65031141/65031141.github.io.git
 cd 65031141.github.io
 git checkout gh-pages
 cd ..
 
+
+# loop
 while [ 0 ]
 do
+  # pull 
   cd 65031141.github.io
   git pull
   cd ..
 
-  mkdir -p html
+  # clear old files
   rm -rf html/*
 
+  # copy new files into html/
   cp -r 65031141.github.io/* html/
 
+  # clear git related files
   rm -rf html/.git
   rm -rf html/.circleci
   rm -rf html/.gitignore
   
+  # fix permission
   chmod -R 755 html
 
+  # mark done.
   echo "updated at $(date)"
 
+  # wait for 1 second
   sleep 1
 done
